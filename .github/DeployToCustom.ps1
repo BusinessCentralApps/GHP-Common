8e621c01297918c765616a77c91bacf345f71427
freddydk <freddydk@users.noreply.github.com>
2024-11-03 08:58:22 +0100

    remove custom

--- .github/DeployToCustom.ps1
+++
@@ -1,7 +0,0 @@
-Param(
-    [Hashtable] $parameters
-)
-write-Host 'Parameters transferred:'
-$parameters | ConvertTo-Json -depth 99 | Out-Host
-
-Add-Content -Encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "environmentUrl=https://www.bing.com"
